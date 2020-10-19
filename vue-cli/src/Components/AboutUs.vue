640a4fc894fe95c7d89475545e99d18056ef8e66
Andrea <andrea-dibra@hotmail.com>
2020-10-19 15:02:27 +0200

    Updating and generating dinamicly with v-for the content of AboutUs component

--- vue-cli/src/Components/AboutUs.vue
+++ vue-cli/src/Components/AboutUs.vue
@@ -6,15 +6,13 @@
       <p style="" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, repudiandae et? Eius doloribus atque libero assumenda deleniti fugiat quia facilis officiis natus! Dolores voluptates illo molestiae consequatur laboriosam. Sit, itaque!</p>
       <h3 class="AboutUsButon" >Find Out More</h3>
     </div>
-    <div class="backDiv"></div>
     <div class="containerWrapper">
 
-
-      <div >
-        <div>
-          <img :src="datats.image" alt="">
-          <h2>datats.header</h2>
-          <p>datats.paragraph</p>
+      <div v-bind:key = index v-for="(item,index) in items" >
+        <div class="container">
+          <img v-bind:src="item.image" alt="">
+          <h2>{{item.header}}</h2>
+          <p>{{item.paragraph}}</p>
 
         </div>
       </div>
@@ -27,54 +25,80 @@
 
 
 export default {
+
   data(){
-    items : [ {
-      image : 'img1.png',
-      header : 'the header',
-      paragraph : 'paragraph'
+  return{
+    items: [{
+      image: '/src/images/1.png',
+      header: 'Lorem ipsum ',
+      paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ea eaque quasi molestiae voluptatem iure ipsam voluptate ipsa, voluptas sed sapiente. Deserunt doloremque, sunt adipisci eos consequatur ea illo explicabo!'
     },
     {
-      image : 'img2.png',
-      header : 'the header',
-      paragraph : 'paragraph'
+      image: '/src/images/2.png',
+      header: 'Lorem ipsum ',
+      paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ea eaque quasi molestiae voluptatem iure ipsam voluptate ipsa, voluptas sed sapiente. Deserunt doloremque, sunt adipisci eos consequatur ea illo explicabo!'
     },
     {
-      image : 'img3.png',
-      header : 'the header',
-      paragraph : 'paragraph'
+      image: '/src/images/3.png',
+      header: 'Lorem ipsum ',
+      paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ea eaque quasi molestiae voluptatem iure ipsam voluptate ipsa, voluptas sed sapiente. Deserunt doloremque, sunt adipisci eos consequatur ea illo explicabo!'
     },
     {
-      image : 'img4.png',
-      header : 'the header',
-      paragraph : 'paragraph'
+      image: '/src/images/4.png',
+      header: 'Lorem ipsum ',
+      paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, ea eaque quasi molestiae voluptatem iure ipsam voluptate ipsa, voluptas sed sapiente. Deserunt doloremque, sunt adipisci eos consequatur ea illo explicabo!'
     }
     ]
   }
+}
   
 }
 </script>
 <style scoped>
 .containerWrapper{
-  height: 1000px;
-  display: flex;
-
+  display:grid;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto auto;
+  column-gap: 40px;
+  row-gap: 40px;
+  justify-content: center;
+  padding-bottom : 140px ;
 }
-.backDiv {
-  height : 500px;
-  position: absolute;
-  width : 100%;
-  background-color:  #575A89 ;
-  display: none;
-
+.containerWrapper > :first-child {
+  margin-top : 60px;
+}
+.containerWrapper > :last-child{
+  margin-top : -60px;
+}
+.container{
+  width:400px;
+  height:300px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow:0  12px 6px rgba(0,0,0,0.2);
+  z-index: 2;
+  position: relative;
+}
+.container > p {
+  color : gray;
+  padding : 20px 15px 
+}
+.container > h2 {
+  color : black;
+  padding :  15px ;
+}
+.container > img{
+  height : 60px;
+  padding : 15px;
 }
 .headWrapper{
   display: felx;
-  padding-top:100px;
+  padding-top:200px;
   text-align: center;
   width : 650px;
   margin-left: auto;
   margin-right : auto;
-  padding-bottom: 100px;
+  padding-bottom: 200px;
 }
 .headWrapper > p {
   padding: 60px;
