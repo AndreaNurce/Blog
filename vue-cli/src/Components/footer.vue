bd05559d726a2df006affeafff21f622753b9012
Andrea <andrea-dibra@hotmail.com>
2020-10-24 20:46:23 +0200

    ...

--- vue-cli/src/Components/footer.vue
+++ vue-cli/src/Components/footer.vue
@@ -9,13 +9,5 @@ export default {
 }
 </script>
 <style scoped>
-.Footer{
-  position:fixed;
-  z-index:-5;  
-  bottom:0;
-  width:100%;
-  background-color: black;
-  height : 500px;
-}
 
 </style>
